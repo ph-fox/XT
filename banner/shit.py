197409c3c239bfef8b8213c14023119f7afd7c82
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2020-12-15 16:14:48 +0800

    Add files via upload

---
+++ banner/shit.py
@@ -0,0 +1,19 @@
+import publicip, os, sys
+
+os.system("clear")
+
+print("""
+██   ██    ████████ 
+ ██ ██      ██    
+  ███  █████ ██    
+ ██ ██     ██    
+██   ██      ██    
+                  
+smart-ip-tracer
+by: Anikin Luke
+""")
+
+print("==================>")
+print("your ip ") 
+publicip.get()
+print("==================>\n")
